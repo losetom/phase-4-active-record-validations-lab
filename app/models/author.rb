785a3f453f7c3ac556540ba5086550c49c7dafb9
Mike Loseto <losetom1998@gmail.com>
2022-11-01 16:45:52 -0400

    Completed Assignment

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,9 @@
 class Author < ApplicationRecord
+    # all authors have a name, no two author have the same name
+
+    validates :name, presence: true, uniqueness: true
+
+    # author phone numbers are exeactly ten digits 
+    
+    validates :phone_number, length: { is: 10 }
 end
